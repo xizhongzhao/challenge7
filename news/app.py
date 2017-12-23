dc540b3583550808a15f47e63ae0498e96249fcb
hades_zhao <hades_zhao@126.com>
2017-12-23 15:08:20 +0800

    3 version

--- news/app.py
+++ news/app.py
@@ -3,42 +3,46 @@
 from flask import Flask,render_template
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-import sys 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/shiyanlou'
 db = SQLAlchemy(app)
-sys.path.append('/home/shiyanlou/challeng7/news')
 
 class File(db.Model):
-    __tablename__ = 'file'
     id = db.Column(db.Integer,primary_key=True,index=True)
     title = db.Column(db.String(80),unique=True)
     created_time = db.Column(db.DateTime,nullable=True)
     category_id = db.Column(db.Integer,db.ForeignKey('category.id'))
-    category = db.relationship('Category',backref=db.backref('posts',lazy='dynamic'))
+    category = db.relationship('Category',backref='files')
     content = db.Column(db.Text)
-
-    def __init__(self,title,created_time,category,content):
+    
+    def __init__(self,title,created_time,category_id,content):
         self.title = title
-        if created_time is None:
-            created_time = created_time
         self.created_time = created_time
-        self.category = category
+        self.category_id = category_id
         self.content = content
-
+    
     def __repr__(self):
-        return '<Article %r>' %self.title
+        return '<File %r>' %self.title
 
 class Category(db.Model):
-    __tablename__ = 'category'
     id = db.Column(db.Integer,primary_key=True,index=True)
     name = db.Column(db.String(80))
-
+    
     def __init__(self,name):
-        self.name=name
+       self.name = name
 
     def __repr__(self):
-        return '<Category %r>' %self.name
+        return '<Category %r>' %self.name 
+
+@app.route('/')
+def index():
+    pass
 
 
+@app.route('/files/<file_id>')
+def file(file_id):
+    pass
+
+if __name__ == '__main__':
+    app.run()
